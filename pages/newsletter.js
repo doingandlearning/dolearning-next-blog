dcf7813b375c704cf4f9e57ea06a54ab3b234c5c
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-01-22 14:14:32 +0000

    Updated newsletter copy.

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -4,17 +4,38 @@ import EmailForm from "@components/EmailForm"
 export default function Garden() {
   return (
     <Layout>
-      <div className="bg-lightPrimary w-full py-16">
+      <div className="bg-white w-full py-16">
         <div className="w-full md:w-1/2 mx-auto px-4">
-          <p className="text-dlblue text-left text-lg font-normal">
-            Updates and cool things from the web
-          </p>
-          <h2 className="w-full md:w-5/6 font-extrabold text-white text-3xl leading-8">
-            Subscribe to the weekly newsletter
+          <h2 className="w-full md:w-5/6 font-extrabold text-lightPrimary text-3xl leading-8">
+            Are you stuck in tutorial hell? Not sure where to go next with your learning?
               </h2>
-          <EmailForm formid="1697448" />
+          <p className="text-dlblue text-left text-lg font-normal">
+            You want to be constantly learning and inspired - growing as a developer and confident that your effort is being well spent.
+          </p>
+          <p>
+
+            Let me help! Sign-up below and every week I'll send you:</p>
+          <ul>
+            <li>
+              an actionable tip that you'll be able to use to level up straight away;
+
+              </li>
+            <li>
+              a curated selection of tutorials that I've worked through myself. I'll share insight on how useful they are and what you can do with the skills gained;
+
+              </li>
+            <li>
+              some of my own videos and writing to help support you on your development journey.
+              </li>
+          </ul>
+
+
+
         </div>
       </div>
-    </Layout>
-  );
+      <div className="bg-lightPrimary w-full py-16 flex mb-10">
+
+        <EmailForm formid="1697448" />
+      </div>
+    </Layout>)
 }
