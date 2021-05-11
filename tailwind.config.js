4036d2b189af89b5f7fdd6375c9fb362600aab70
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-05-11 18:44:32 +0100

    Fixes tailwind error.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: {
-    // content: [
-    //   './pages/**/*.js',
-    //   './node_modules/flatpickr/**/*.js',
-    // ],
-  },
+  purge: [],
   theme: {
     extend: {
       fontSize: {
