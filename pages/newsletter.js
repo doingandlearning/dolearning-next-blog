23dbf375db9fcb5a6aa8f4925f348b79e84ca2ef
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-01-10 10:09:57 +0000

    Updates the newsletter page.

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,5 +1,5 @@
 import Layout from "@components/Layout";
-import EmailForm from "@components/EmailForm"
+import EmailForm from "@components/EmailForm";
 
 export default function Garden() {
   return (
@@ -7,35 +7,37 @@ export default function Garden() {
       <div className="bg-white w-full py-16">
         <div className="w-full md:w-1/2 mx-auto px-4">
           <h2 className="w-full md:w-5/6 font-extrabold text-lightPrimary text-3xl leading-8">
-            Stuck in tutorial hell?
-              </h2>
+            Doing and Learning
+          </h2>
           <p className="text-dlblue text-left text-lg font-normal">
-            You want to be constantly learning and inspired - growing as a developer and confident that your effort is being well spent.
+            I build my life around those two actions and am fascinated by how
+            they interact with each other. Doing informs learning which informs
+            doing and on and on.
           </p>
           <p>
-
-            Let me help! Sign-up below and every week I'll send you:</p>
+            I write a weekly newsletter that helps encourage you on your
+            learning journey, as well as sharing helpful resources and cool
+            finds from the web. Sign up below so that each Monday you'll get:
+          </p>
           <ul className="leading-6">
             <li>
-              an actionable tip that you'll be able to use to level up straight away;
-
-              </li>
+              an actionable and helpful reflection to help reflect on and level
+              up your learning;
+            </li>
+            <li>two learning resources that can help you on your way;</li>
             <li>
-              a curated selection of tutorials that I've worked through myself. I'll share insight on how useful they are and what you can do with the skills gained;
-
-              </li>
-            <li>
-              some of my own videos and writing to help support you on your development journey.
-              </li>
+              and three cool things I've found while exploring the internet.
+            </li>
           </ul>
-
-
-
+          <p>
+            I'll also shout out some things I've been working on that I think
+            you'll enjoy and find valuable.
+          </p>
         </div>
       </div>
       <div className="bg-lightPrimary w-full py-4 flex mb-10">
-
         <EmailForm formid="1697448" />
       </div>
-    </Layout>)
+    </Layout>
+  );
 }
